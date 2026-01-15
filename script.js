e43d023ae02e5c6e3452d179e75506c02bd92bff
puja843 <pujakumarijsr282@gmail.com>
2026-01-15 22:37:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
